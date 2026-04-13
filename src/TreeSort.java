4a476f154439e4ef44a855bdec5a70a334bb1784
Syn-1ax <shyneg123@github.com>
2026-04-14 00:29:32 +0100

    First Rendition of the Tree Sort Program WITHOUT TESTER

--- src/TreeSort.java
+++ src/TreeSort.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
 public class TreeSort {
-    
+
+    // Sort Using Tree Sort
+    public static ArrayList<Integer> sort(ArrayList<Integer> x){
+        
+        // First Start At The Root
+        Node root = new Node(x.get(0));
+
+        // Continuously add Numbers to the Root Node
+        for(int i = 1 ; i < x.size(); i++){
+            root.add_item(x.get(i));
+        }
+
+        // Walk and return the tree list, but now sorted.
+        return root.walk();
+    }
+    public static void main(String[] args){
+        
+    }
 }
