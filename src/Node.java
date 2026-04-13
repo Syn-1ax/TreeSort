450b4c0f49872f7304d143c624d829f6b2c40d41
Syn-1ax <shyneg123@github.com>
2026-04-13 17:49:53 +0100

    .

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,11 @@
+
+// Nodes being the point where the numbers are
 public class Node {
+
+    // Array Store
+    
+
+
     public static void main(String[] args){
-        System.out.println("Test Commit, Making Sure It Works.");
     }
 }
