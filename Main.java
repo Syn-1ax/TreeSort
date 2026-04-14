592668c08f41059025121c71c9825d8060c53990
Syn-1ax <shyneg123@github.com>
2026-04-14 01:31:56 +0100

    Finished The Main User Input Scanner (First Version)

---
+++ Main.java
@@ -0,0 +1,11 @@
+package src;
+import TreeSort;
+import Node;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+    
+    // 
+}
---
+++ MainUSERINPUT.java
@@ -0,0 +1,32 @@
+package src;
+import TreeSort;
+import Node;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// This Version of The File Is Used For if you want to input the numbers manually
+// Basically, if you dont have some copy paste of the list, but want to test random numbers
+public class MainUSERINPUT {
+    public static void main(String[] args){
+    
+    // Manual Inputter:
+        Scanner UserInput = new Scanner(System.in);
+        ArrayList<Integer> List = new ArrayList<>();
+
+
+    System.out.print("Amount of Numbers In List? ");
+    int n = scanner.nextInt();
+
+    System.out.println("Enter " + n + " Numbers:");
+    for (int i = 0; i < n; i++){
+        System.out.print("Number " + i + ":");
+        List.add(scanner.nextInt());
+    }
+
+    System.out.println("Unsorted List: " + List);
+    System.out.println("Sorted List: " + TreeSort.sort(List));
+    scanner.close();
+    }
+}
