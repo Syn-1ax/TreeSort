ad6b8eb2c913123487b4c686339b426caab82ac4
Syn-1ax <shyneg123@github.com>
2026-04-14 19:19:32 +0100

    The Creation of the Super Simple Copy-Paste Main File that doesnt ask for manual insertion of Arrays

--- Main.java
+++ Main.java
@@ -6,7 +6,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
     
-        // 
-        ArrayList<Integer> List = new ArrayList<>(Arrays.asList())
+        // CHANGE ARRAY HERE IF USING THIS METHOD
+        ArrayList<Integer> List = new ArrayList<>(Arrays.asList(4,2,3,5,1)); // Chane the Values in the asList part, comma spaced, to paste your own Array instead.
+        ArrayList<Integer> SortedList = TreeSort.sort(List);
+
+        System.out.println("Unsorted List: " + List);
+        System.out.println("Sorted List: " + SortedList);
     }
 }
